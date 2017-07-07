a37928a2e7f6ebd1277f25c17c6c9e6a040ecfb9
Anhtu07 <tu.nguyenanh.96@gmail.com>
2017-07-07 22:11:14 +0700

    final

--- js/index.js
+++ js/index.js
@@ -2,35 +2,37 @@ var number_of_digits = 0;
 var prev_value;
 var current_value;
 var op;
-var max = Math.pow(10, 13);
+var max = Math.pow(10, 10);
 console.log(max);
 function clear() {
   number_of_digits = 0;
   op = undefined;
-  $("#display").html("");
+  $("#input").html("");
 }
 
 $("button").click(function() {
-  if (!isNaN(this.id) && number_of_digits <= 13) {
-    var current = $("#display").html();
+  if (!isNaN(this.id) && number_of_digits <= 10) {
+    var current = $("#input").html();
     current += this.id;
-    $("#display").html(current);
+    $("#input").html(current);
     number_of_digits++;
   } else if (this.id === "clear") {
+    $("#current_op").html("none");
     clear();
   } else if (this.id === "delete") {
-    var current = $("#display").html();
+    var current = $("#input").html();
     current = current.substring(0, current.length - 1);
     number_of_digits--;
-    $("#display").html(current);
+    $("#input").html(current);
   } else if (this.id === "decimal") {
-    var current = $("#display").html();
+    var current = $("#input").html();
     current += ".";
-    $("#display").html(current);
+    $("#input").html(current);
   } else if (this.id === "equal") {
+    $("#current_op").html("result");
     if (prev_value !== undefined) {
       var result;
-      current_value = parseFloat($("#display").html());
+      current_value = parseFloat($("#input").html());
       if (op === "plus") {
         result = prev_value + current_value;
       } else if (op === "minus") {
@@ -43,7 +45,15 @@ $("button").click(function() {
       clear();
       prev_value = undefined;
       current_value = undefined;
-      $("#display").html(result);
+      if (result >= max) {
+        clear();
+        prev_value = undefined;
+        current_value = undefined;
+        $("#input").html("ENORMOUS !!!");
+        return;
+      }
+      result = result.toFixed(2);
+      $("#input").html(result);
     }
   }
 });
@@ -51,9 +61,9 @@ $("button").click(function() {
 $(".op").click(function() {
   var result;
   if (prev_value === undefined) {
-    prev_value = parseFloat($("#display").html());
+    prev_value = parseFloat($("#input").html());
   } else {
-    current_value = parseFloat($("#display").html());
+    current_value = parseFloat($("#input").html());
     if (op === "plus") {
       result = prev_value + current_value;
     } else if (op === "minus") {
@@ -65,15 +75,14 @@ $(".op").click(function() {
     }
     prev_value = result;
   }
-  console.log(result);
-  if(result >= max)
-    {
-      clear();
-      prev_value = undefined;
-      current_value = undefined;
-      $("#display").html("ENORMOUS !!!");
-      return;
-    }
+  $("#current_op").html(this.id);
+  if (result >= max) {
+    clear();
+    prev_value = undefined;
+    current_value = undefined;
+    $("#input").html("ENORMOUS !!!");
+    return;
+  }
   clear();
   op = this.id;
 });
